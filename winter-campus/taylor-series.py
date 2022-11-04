b8bb120ee0fa3d4afba99e885751458568b63d0c
Aayush Joglekar <aayushjog@gmail.com>
2022-11-04 20:14:13 +0100

    Add TaylorSeries

--- winter-campus/taylor-series.py
+++ winter-campus/taylor-series.py
@@ -129,3 +129,100 @@ class GraphBasics(Scene):
         self.wait()
         self.play(x.animate.set_value(4), run_time=3)
         self.wait()
+
+
+class TaylorSeries(Scene):
+    def construct(self):
+        title = Text("Taylor Series")
+        self.play(Write(title))
+        self.wait()
+
+        self.play(title.animate.scale(0.8).to_edge(UP, buff=1))
+        self.wait()
+
+        description = Text(
+            "The Taylor series of a real or complex-valued function f(x) \nthat is infinitely differentiable at a real or complex number a is the power series:", font_size=24)
+        self.play(Write(description))
+        self.wait()
+        self.play(description.animate.shift(UP*1.5))
+        self.wait()
+        long_formula = MathTex("g(x)=", "\\frac{f^{(0)}(x-a)}{0!}", "(x-a)^0", "+", "\\frac{f^{(1)}(x-a)}{1!}", "(x-a)^1", "+", "\\frac{f^{(2)}(x-a)}{2!}",
+                               "(x-a)^2", "+", "\\frac{f^{(3)}(x-a)}{3!}", "(x-a)^3", "+", "...", font_size=24)
+
+        colors = [YELLOW, PURPLE_A, RED, ORANGE]
+
+        for i, color in enumerate(colors):
+            long_formula[1 + 3*i].set_color(color)
+
+        self.play(Write(long_formula))
+        self.wait()
+
+        mclauren_formula = MathTex("g(x)=", "\\frac{f(0)}{0!}", "x^0", "+", "\\frac{f^{'}(0)}{1!}", "x^1", "+", "\\frac{f^{''}(0)}{2!}",
+                                   "x^2", "+", "\\frac{f^{'''}(0)}{3!}", "x^3", "+", "...", font_size=28).move_to(long_formula.get_center())
+
+        for i, color in enumerate(colors):
+            mclauren_formula[1 + 3*i].set_color(color)
+
+        self.play(ReplacementTransform(long_formula, mclauren_formula))
+        self.wait()
+
+        title_new = Text("Maclaurin Series").scale(
+            0.8).move_to(title.get_center())
+        self.play(ReplacementTransform(title, title_new), FadeOut(description))
+        self.wait()
+
+        self.play(FadeOut(title_new), mclauren_formula.animate.to_edge(UR))
+        self.wait()
+
+        func_text = MathTex("g(x)", "=", "cos(x)")
+        func_text[0].set_color(TEAL)
+        self.play(Write(func_text))
+        self.wait()
+        self.play(func_text.animate.scale(0.7).to_edge(
+            UL, buff=DEFAULT_MOBJECT_TO_EDGE_BUFFER * 1.5))
+        self.wait()
+
+        plane = NumberPlane(x_length=10, y_length=4,
+                            x_range=[-5, 5], y_range=[-2, 2], axis_config={"include_numbers": True, "font_size": 16, "stroke_color": GRAY}, background_line_style={
+                                "stroke_color": GRAY_D})
+
+        self.play(DrawBorderThenFill(plane))
+        self.wait()
+
+        def func_cos(x): return np.cos(x)
+        plot_cos = plane.plot(
+            func_cos, x_range=[-5, 5], stroke_color=TEAL, stroke_opacity=0.75)
+        self.play(Create(plot_cos, run_time=3))
+        self.wait()
+
+        p0_coeff = MathTex("1").set_color(YELLOW).scale(
+            0.75).move_to(mclauren_formula[1].get_center())
+        self.play(ReplacementTransform(mclauren_formula[1], p0_coeff))
+        self.wait(2)
+
+        def p0_func(x): return 1
+        p0_graph = plane.plot(p0_func, x_range=[-5, 5], stroke_color=YELLOW)
+        self.play(Create(p0_graph, run_time=3))
+        self.wait(2)
+
+        p1_coeff = MathTex("0").set_color(PURPLE_A).scale(
+            0.75).move_to(mclauren_formula[4].get_center())
+        self.play(ReplacementTransform(mclauren_formula[4], p1_coeff))
+        self.wait(2)
+
+        def p1_func(x): return 1
+        p1_graph = plane.plot(p1_func, x_range=[-5, 5], stroke_color=PURPLE_A)
+        self.play(ReplacementTransform(p0_graph, p1_graph))
+        self.wait(2)
+
+        p2_group = VGroup(mclauren_formula[6], mclauren_formula[7])
+
+        p2_coeff = MathTex("-", "1").set_color(RED).scale(
+            0.8).move_to(p2_group.get_center())
+        self.play(ReplacementTransform(p2_group, p2_coeff))
+        self.wait(2)
+
+        def p2_func(x): return 1 - x**2
+        p2_graph = plane.plot(p2_func, x_range=[-5, 5], stroke_color=RED)
+        self.play(ReplacementTransform(p1_graph, p2_graph))
+        self.wait(2)
